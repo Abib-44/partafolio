2bbf68964e0a8a68f6e9a129245634e482133f04
el <eh.benaboud.dev@gmail.com>
2026-01-13 16:23:53 +0100

    first commit

---
+++ assets/js/load-nav-footer.js
@@ -0,0 +1,26 @@
+function includeHTML(id, url) {
+  return fetch(url)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(id).innerHTML = data;
+    })
+    .catch(err => console.error(`Error loading ${url}:`, err));
+}
+
+Promise.all([
+  includeHTML("navbar-placeholder", "../../_includes/navbar.html"),
+  includeHTML("footer-placeholder", "../../_includes/footer.html")
+]).then(() => {
+  const path = window.location.pathname;
+  const page = path.split("/").pop().replace(".html", "");
+
+  const titles = {
+    "index": "Benaboud | Accueil",
+    "competences": "Compétences",
+    "projects": "Projets",
+    "contact": "Contactez-moi",
+    "hobbies": "Loisirs"
+  };
+
+  document.title = titles[page] || "Benaboud";
+});
