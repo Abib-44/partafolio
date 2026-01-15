ee2c537bffd675494f8dda055d13ee5ea18deea4
el <eh.benaboud.dev@gmail.com>
2026-01-15 21:41:55 +0100

    d

--- assets/js/load-nav-footer.js
+++ assets/js/load-nav-footer.js
@@ -1,25 +1,40 @@
-const loadNavbar = () => {
-  const currentUrl = new URL(window.location.href);
-  
-  if (currentUrl.pathname == '/index.html') {
-    nav = './assets/partials/navbar.html'
-  } else {
-    nav = '../assets/partials/navbar.html'
-  }
-  fetch(nav)
-    .then(response => {
-      if (!response.ok) throw new Error(`Errore nel caricamento: ${response.status}`);
-      return response.text();
+function loadPartial(id, path) {
+  fetch(path)
+    .then(r => {
+      if (!r.ok) throw new Error(`Errore nel caricamento: ${r.status}`);
+      return r.text();
     })
     .then(html => {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(html, 'text/html');
-      const placeholder = document.querySelector('#navbar-placeholder');
+      const doc = new DOMParser().parseFromString(html, 'text/html');
+      const placeholder = document.querySelector(`#${id}`);
       if (!placeholder) return;
-
       doc.body.childNodes.forEach(node => placeholder.append(node));
     })
     .catch(err => console.error(err));
-};
+}
 
-document.addEventListener('DOMContentLoaded', loadNavbar);
+document.addEventListener('DOMContentLoaded', () => {
+  const currentUrl = new URL(window.location.href);
+  let navPath, footerPath;
+
+  if (currentUrl.protocol == "https:") {
+    if (currentUrl.pathname === '/' || currentUrl.pathname.endsWith('/index.html')) {
+      navPath = '../assets/partials/navbar.html';
+      footerPath = '../assets/partials/footer.html';
+    } else {
+      navPath = '../../assets/partials/navbar.html';
+      footerPath = '../../assets/partials/footer.html';
+    }
+  }
+
+  if (currentUrl.pathname === '/' || currentUrl.pathname.endsWith('/index.html')) {
+    navPath = './assets/partials/navbar.html';
+    footerPath = './assets/partials/footer.html';
+  } else {
+    navPath = '../assets/partials/navbar.html';
+    footerPath = '../assets/partials/footer.html';
+  }
+
+  loadPartial('navbar-placeholder', navPath);
+  loadPartial('footer-placeholder', footerPath);
+});
