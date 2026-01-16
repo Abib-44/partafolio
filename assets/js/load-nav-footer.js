facfe573cf1a82fa0d0524a6a2b990954e866808
el <eh.benaboud.dev@gmail.com>
2026-01-16 11:39:45 +0100

    d

--- assets/js/load-nav-footer.js
+++ assets/js/load-nav-footer.js
@@ -41,39 +41,14 @@ function replaceNavLinks() {
       link.setAttribute('href', '/portfolio/pages/contact.html');
     } else if (href.includes('hobbies.html')) {
       link.setAttribute('href', '/portfolio/pages/hobbies.html');
+    } else if (href.includes('avatar.jpeg')) {
+      link.setAttribute('href', '/portfolio/assets/img/avatar.jpeg');
     }
 
     console.log(`Link ${i} dopo:`, link.getAttribute('href'));
   });
 }
-function replaceNavLinksLocal() {
-  const navLinks = document.querySelectorAll('.nav__link');
 
-  console.log('Nav links trovati:', navLinks.length);
-
-  navLinks.forEach((link, i) => {
-    const href = link.getAttribute('href');
-    console.log(`Link ${i} prima:`, href);
-
-    console.log("locale");
-    
-    if (!href) return;
-
-    if (href.includes('index.html')) {
-      link.setAttribute('href', '/index.html');
-    } else if (href.includes('competences.html')) {
-      link.setAttribute('href', '/pages/competences.html');
-    } else if (href.includes('projects.html')) {
-      link.setAttribute('href', '/pages/projects.html');
-    } else if (href.includes('contact.html')) {
-      link.setAttribute('href', '/pages/contact.html');
-    } else if (href.includes('hobbies.html')) {
-      link.setAttribute('href', '/pages/hobbies.html');
-    }
-
-    console.log(`Link ${i} dopo:`, link.getAttribute('href'));
-  });
-}
 document.addEventListener('DOMContentLoaded', async () => {
   await loadPartial('navbar-placeholder', 'navbar.html');
   await loadPartial('footer-placeholder', 'footer.html');
@@ -81,7 +56,6 @@ document.addEventListener('DOMContentLoaded', async () => {
   const currentUrl = new URL(window.location.href);
   if (currentUrl.protocol == "https:") {
     replaceNavLinks();
-  } else {
-    replaceNavLinksLocal()
   }
+
 });
